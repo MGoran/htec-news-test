1932133e0a4b3f25b7093b74e0eaf75ff7cbf820
MGoran <goranmaslic92@gmail.com>
2020-08-06 23:18:19 +0200

    test: fix failing tests
    - caused by using store actions

--- src/app/shell/header/header.component.spec.ts
+++ src/app/shell/header/header.component.spec.ts
@@ -5,16 +5,23 @@ import { RouterTestingModule } from '@angular/router/testing';
 
 import { I18nModule } from '@app/i18n';
 import { HeaderComponent } from './header.component';
+import { CountrySwitcherComponent } from '@app/ui-components/country-switcher/country-switcher.component';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+
+import { initialState } from '@app/store/reducer';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let store: MockStore;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule, NgbModule, TranslateModule.forRoot(), I18nModule],
-      declarations: [HeaderComponent],
-      providers: [],
+      declarations: [HeaderComponent, CountrySwitcherComponent],
+      providers: [
+        provideMockStore({ initialState }),
+      ],
     }).compileComponents();
   }));
 
@@ -22,6 +29,7 @@ describe('HeaderComponent', () => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    store = TestBed.inject(MockStore);
   });
 
   it('should create', () => {
--- src/app/ui-components/country-switcher/country-switcher.component.spec.ts
+++ src/app/ui-components/country-switcher/country-switcher.component.spec.ts
@@ -1,14 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 import { CountrySwitcherComponent } from './country-switcher.component';
+import { initialState } from '@app/store/reducer';
 
 describe('CountrySwitcherComponent', () => {
   let component: CountrySwitcherComponent;
   let fixture: ComponentFixture<CountrySwitcherComponent>;
+  let store: MockStore;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [CountrySwitcherComponent],
+      providers: [
+        provideMockStore({ initialState }),
+      ],
+
     }).compileComponents();
   }));
 
@@ -16,6 +23,7 @@ describe('CountrySwitcherComponent', () => {
     fixture = TestBed.createComponent(CountrySwitcherComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    store = TestBed.inject(MockStore);
   });
 
   it('should create', () => {
