9d7990842368c965849c009ab6662ec4c673f003
MGoran <goranmaslic92@gmail.com>
2020-08-08 16:17:17 +0200

    chore(linter): line too long

--- src/app/store/actions.ts
+++ src/app/store/actions.ts
@@ -2,7 +2,11 @@ import { createAction, props } from '@ngrx/store';
 import { Countries, Loader } from './reducer';
 import { NewsArticle } from '@app/news/service/news-api.service';
 
-export const changeCountry = createAction('[Global] Change country', props<{ country: Countries }>());
-export const toggleLoader = createAction('[Global] Toggle loader', props<Loader>());
-export const toggleCountrySwitcher = createAction('[Global] Disable/Enable country switcher', props<{ disableCountrySwitcher: boolean }>());
-export const loadNews = createAction('[News] Load News', props<{ [key: string]: NewsArticle[] }>());
+export const changeCountry
+    = createAction('[Global] Change country', props<{ country: Countries }>());
+export const toggleLoader
+    = createAction('[Global] Toggle loader', props<Loader>());
+export const toggleCountrySwitcher
+    = createAction('[Global] Disable/Enable country switcher', props<{ disableCountrySwitcher: boolean }>());
+export const loadNews
+    = createAction('[News] Load News', props<{ [key: string]: NewsArticle[] }>());
